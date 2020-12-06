ae755e056dc62dd495495419324a4809275c491e
Vadym Martsynovskyy <vmartsynovskyy@gmail.com>
2020-12-06 11:45:20 -0800

    Add prepare project script

---
+++ prepare_project.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# scripted tested on Ubuntu 20.04
+# clone submodules
+git submodule update --init --recursive
+
+# install dependencies
+sudo apt-get update && sudo apt-get upgrade && sudo apt-get install ffmpeg gpac docker git
+pip install gdown
+
+# download videos
+mkdir -p videos
+pushd videos
+
+# tom.mp4
+gdown --id 1bGYfgrziLQi7ZwgFF2GrID1qUZh3ONqW
+
+# convert videos for DASH
+./mp4_to_dash.sh tom
+
+popd
+
+# compile GPAC fork
+pushd gpac
+
+./configure && make -j
+
+popd
+
